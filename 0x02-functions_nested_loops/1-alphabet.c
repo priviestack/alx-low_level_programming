16f89d6372cc80f33d29ca3b252c64aa0413e68f
Privilege <privilegemupindu@gmail.com>
2022-10-18 15:24:47 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; a++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
