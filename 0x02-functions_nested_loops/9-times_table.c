9565b0e7edf38ae9eb17bdf6dc52de2d83c0858b
Privilege <privilegemupindu@gmail.com>
2022-10-19 19:01:52 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  */
 void times_table(void)
 {
-	int, num, mult, prod;
+	int num, mult, prod;
 
 	for (num = 0; num <= 9; mult++)
 	{
@@ -17,9 +17,13 @@ void times_table(void)
 			_putchar(' ');
 
 			if (prod <= 9)
+			{
 				_putchar(' ');
+			}
 			else
+			{
 				_putchar((prod / 10) + '0');
+			}
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
