15376bb1de338324356762731a51c1fce5e1bc4b
Privilege <privilegemupindu@gmail.com>
2022-10-20 15:12:30 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,11 @@ int main(void)
 	for (n = 2; n <= 100; n++)
 	{
 		printf(" ");
-		if (n % 5 == 0)
+		if (n % 3 == 0)
 			printf("Fizz");
 		if (n % 5 == 0)
 			printf("Buzz");
-		if (n % 3 != 0 && % 5 != 0)
+		if (n % 3 != 0 && n % 5 != 0)
 			printf("%d", n);
 	}
 	printf("\n");
