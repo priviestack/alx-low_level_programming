5b98cc1b720e5afcb5870d1f83d8d882f4bc66dd
Privilege <privilegemupindu@gmail.com>
2022-10-24 13:10:33 -0700

    swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values of two integers
+ * @a: int a
+ * @b: int b
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int q:
+
+		q = *a;
+		*a = *b;
+		*b = *q;
+}
