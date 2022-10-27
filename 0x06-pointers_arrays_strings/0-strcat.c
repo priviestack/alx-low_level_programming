6d16d3669b8546237f9b11135edb912e21a5470d
Privilege <privilegemupindu@gmail.com>
2022-10-27 12:24:25 -0700

    more arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,14 +12,14 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[1] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[1] = src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
