4e939327ada2d279b07781e591567dfc8e9b2c4c
Privilege <privilegemupindu@gmail.com>
2022-10-31 22:16:06 -0700

    print diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,7 +10,9 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum 2 = 0;
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for (i = 0; i < size * size; i++)
 	{
@@ -18,9 +20,9 @@ void print_diagsums(int *a, int size)
 		{
 			sum1 += a[i];
 		}
+
 		if (i % (size - 1) == 0 && (i > 0 && (i < (size * size - 1))))
 		{
-
 			sum2 += a[i];
 		}
 	}
